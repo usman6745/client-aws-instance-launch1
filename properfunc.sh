161a7df71ae41eec99fdec1bafd83310280b663a
usman6745 <33342456+usman6745@users.noreply.github.com>
2019-03-17 12:25:18 +0530

    Update properfunc.sh

--- properfunc.sh
+++ properfunc.sh
@@ -2,34 +2,39 @@
 INITIAL_ARGS="$*"
 launch_ec2() {
    local AMI_ID="$1"
-   local KEYPAIR="$2"
-   local SUBNET="$3"
-   local REGION="$4"
-   local TAG_KEY="$5"
-   local TAG_VALUE="$6"
-   local INSTANCE_TYPE="$7"
-   local SECURITY_GROUP_ID="$8"
+   local SUBNET="$2"
+   local REGION="$3"
+   local NAME="$4"
+   local NAME_VALUE="$5"
+   local Number_Of_Instances="$6"
+   local SECURITY_GROUP_ID="$7"
+   local KEYPAIR="$8"
+   local INSTANCE_TYPE="$9"
    {
        echo """Your instance details is:-
        AMI_ID="${AMI_ID}"
-       KEYPAIR_NAME="${KEYPAIR}"
+       KEY_NAME="${NAME}"
+       KEY_VALUE="${NAME_VALUE}"
        SUBNET_NAME="${SUBNET}"
        REGION_NAME="${REGION}"
-       TAG_KEY="${TAG_KEY}"
-       TAG_VALUE="${TAG_VALUE}"
+       NUMBER_OF_INSTANCES="${Number_Of_Instances}"
+       SECURITY_GROUP_ID="${SECURITY_GROUP_ID}"
+       KEYPAIR="${KEYPAIR}"
+       INSTANCE_TYPE="${INSTANCE_TYPE}"     
        """
    }
-#echo "aws ec2 run-instances --image-id "${AMI_ID}" --count 1 --instance-type "${INSTANCE_TYPE}" --key-name "${KEYPAIR}" --security-group-ids "${SECURITY_GROUP_ID}" --subnet-id "${SUBNET}" --region "${REGION}""
-aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type ${INSTANCE_TYPE} --key-name ${KEYPAIR} --security-group-ids ${SECURITY_GROUP_ID} --subnet-id ${SUBNET} --region ${REGION} > ec2.txt
+echo "aws ec2 run-instances --image-id ${AMI_ID} --count $Number_Of_Instances  --instance-type ${INSTANCE_TYPE} --key-name ${KEYPAIR} --security-group-ids ${SECURITY_GROUP_ID} --subnet-id ${SUBNET} --region ${REGION}"
+aws ec2 run-instances --image-id ${AMI_ID} --count $Number_Of_Instances  --instance-type ${INSTANCE_TYPE} --key-name ${KEYPAIR} --security-group-ids ${SECURITY_GROUP_ID} --subnet-id ${SUBNET} --region ${REGION} > ec2.txt
 grep InstanceId ec2.txt | tr -d '", "' > InstanceId
 grep KeyName ec2.txt | tr -d '", "' > KeyName
 sed -i 's/:/=/g' InstanceId
-echo "aws ec2 create-tags --resources ""$"InstanceId" --tags Key=""$"TAG_KEY",Value=""$"TAG_VALUE" --region $REGION" >> InstanceId
+echo "aws ec2 create-tags --resources ""$"InstanceId" --tags Key=""$"NAME",Value=""$"NAME_VALUE" --region $REGION" >> InstanceId
 chmod +x InstanceId
 ./InstanceId
 id=`head -1 InstanceId`
 id2=`head -1 KeyName`
-#echo "$id"
+echo "$id"
+echo "$id2"
 echo " Instance is launched"
 }
 #launch_ec2 $AMI_ID $KEYPAIR $SUBNET $REGION $TAG_KEY $TAG_VALUE $INSTANCE_TYPE $SECURITY_GROUP_ID
